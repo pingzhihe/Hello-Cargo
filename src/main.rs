faba50c74e962da82d5f5fe0c2166745c655af90
pingzhihe <pzh1760473545@gmail.com>
2023-10-11 18:22:01 +1100

    inital try

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+
+use std::io; //prelude
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guessing Game!");
+
+    println!("Guess a number between 1 and 100");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+
+
 }
